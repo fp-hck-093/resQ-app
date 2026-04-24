58c0201c5635bc4148a91093d6808731c7627291
erwindW99 <erwindwiranata17@gmail.com>
2026-04-24 14:57:05 +0700

    feat(server): set up initial application structure with environment variables, GraphQL, and MongoDB integration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongoloquentModule } from '@mongoloquent/nestjs';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongoloquentModule.forRoot({
+      connection: process.env.MONGO_URI ?? 'mongodb://localhost:27017',
+      database: process.env.DB_NAME ?? 'resq-db',
+      timezone: 'Asia/Jakarta',
+      global: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      subscriptions: {
+        'graphql-ws': true,
+      },
+    }),
+  ],
+  providers: [AppResolver],
 })
 export class AppModule {}
